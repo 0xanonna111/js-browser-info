eb129816fb1d4f38c32a35bb7d698c1498e4bd9d
0xanonna111 <136353692+0xanonna111@users.noreply.github.com>
2026-01-06 12:02:48 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("browser").innerText = navigator.userAgent;
+document.getElementById("os").innerText = navigator.platform;
+document.getElementById("screen").innerText =
+  `${screen.width} x ${screen.height}`;
+document.getElementById("lang").innerText = navigator.language;
